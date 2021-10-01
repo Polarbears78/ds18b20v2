d9ffc1969e725154c6cd674528e946059f7890ee
Polarbears78 <91547292+Polarbears78@users.noreply.github.com>
2021-10-01 11:15:01 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-let t1 = 0
 let t2 = 0
+let t1 = 0
+OLED.init(128, 64)
+OLED.clear()
 basic.forever(function () {
     t1 = DS18B20.TemperatureNumber(DS18B20.pin.pin0)
     if (Math.abs(t1 - t2) < 0.3) {
-        led.plotBarGraph(
-        t1,
-        100
-        )
-        basic.pause(100)
+        OLED.writeNumNewLine(t1)
+        OLED.newLine()
+        basic.pause(1000)
     }
     t2 = t1
 })

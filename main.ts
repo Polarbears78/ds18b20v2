918ed45a67cd2c82baba9a57a87978f62eefc9c8
Polarbears78 <91547292+Polarbears78@users.noreply.github.com>
2021-10-01 11:08:15 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,13 +1,1 @@
-let t1 = 0
-let t2 = 0
-basic.forever(function () {
-    t1 = DS18B20.TemperatureNumber(DS18B20.pin.pin0)
-    if (Math.abs(t1 - t2) < 0.3) {
-        led.plotBarGraph(
-        t1,
-        100
-        )
-        basic.pause(100)
-    }
-    t2 = t1
-})
+
